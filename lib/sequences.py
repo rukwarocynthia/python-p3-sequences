3251fccd5817a7774dfdd0def59208e23af7b433
rukwarocynthia4093@gmail.com <rukwarocynthia4093@gmail.com>
2024-05-26 16:15:58 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <0:
+        return []
+    
+    fibonacci_sequence = []
+
+    for i in range(length):
+        if i == 0:
+            fibonacci_sequence.append(0)
+        elif i == 1:
+            fibonacci_sequence.append(1)
+        else:
+            next_number = fibonacci_sequence[-1] +fibonacci_sequence[-2] 
+            fibonacci_sequence.append(next_number)   
+
+    if length == 0:
+        print('[]')
+    else:
+        print(fibonacci_sequence)
+
+    return fibonacci_sequence                    
